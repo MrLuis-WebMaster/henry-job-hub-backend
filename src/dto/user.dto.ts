3b73476d0323544121c573af578e43723193689c
Luis Martinez <mr.luiswebmaster@gmail.com>
2023-12-12 10:02:03 -0500

    feat: :sparkles: credential validation

--- src/dto/user.dto.ts
+++ src/dto/user.dto.ts
@@ -1,11 +1,27 @@
-import { IsEmail, IsNotEmpty, IsEnum } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsEnum, IsStrongPassword } from 'class-validator';
 import { UserRole } from '../schemas/user.schema';
 
-export class CreateUserDto {
+class CredentialsUserDto {
+  @IsNotEmpty()
   @IsEmail()
   email: string;
+
   @IsNotEmpty()
+  @IsStrongPassword({
+    minLength: 8,
+    minLowercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+    minUppercase: 1,
+  })
   password: string;
+}
+
+export class CreateUserDto extends CredentialsUserDto {
+  @IsNotEmpty()
+  name: string;
+  @IsNotEmpty()
+  lastName: string;
 }
 
 export class CreateAdminDto extends CreateUserDto {
@@ -14,4 +30,4 @@ export class CreateAdminDto extends CreateUserDto {
   role: UserRole;
 }
 
-export class LoginUserDto extends CreateUserDto {}
+export class LoginUserDto extends CredentialsUserDto {}
