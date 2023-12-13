1e8f0c2a7a96b73da9487bc80b8dbcb71b17ca45
Francisco Cedermaz <franciscocedermaz@MacBook-Pro-de-Admin.local>
2023-12-12 21:35:33 -0300

    feat: add get user data route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
 import { CreateUserDto, CreateAdminDto, LoginUserDto } from 'src/dto/user.dto';
 import { AuthService } from './auth.service';
 import { Public } from './decorators/public.decorator';
+import { JwtService } from '@nestjs/jwt';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly jwtService: JwtService,
+  ) {}
   @Public()
   @Post('register/user')
   registerUser(@Body() createUserDto: CreateUserDto) {
@@ -20,5 +24,16 @@ export class AuthController {
   @Post('login')
   login(@Body() loginUserDto: LoginUserDto) {
     return this.authService.login(loginUserDto);
+  }
+  @Public()
+  @Get('user')
+  getUserInfo(@Req() req) {
+    try {
+      const token = req.headers.authorization.split(' ')[1];
+      const decoded = this.jwtService.verify(token);
+      return this.authService.getUserInfo(decoded.email);
+    } catch (error) {
+      return { message: 'Invalid or expired token' };
+    }
   }
 }
