b40b1de9b01d9dfeb1bc8fcc1b58f8e076c4700b
Luis Martinez <mr.luiswebmaster@gmail.com>
2023-12-23 14:42:09 -0500

    feat: :sparkles:

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,26 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { JobOpportunityModule } from './job-opportunity/job-opportunity.module';
 import { MailModule } from './mail/mail.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-yet';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DB_URL),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      useFactory: async () => ({
+        store: await redisStore({
+          socket: {
+            host: process.env.REDIS_HOST || 'localhost',
+            port: (process.env.REDIS_PORT as unknown as number) || 6379,
+          },
+          username: process.env.REDIS_USERNAME,
+          password: process.env.REDIS_PASSWORD,
+        }),
+      }),
+    }),
     JobOpportunityModule,
     AuthModule,
     MailModule,
